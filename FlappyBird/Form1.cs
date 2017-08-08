f070a5c7ba4b759891c434844a70e77a2ec45f30
Abel Berhane <aberhane@datadimensions.com>
2017-08-08 16:18:26 -0400

    Flushed out the comments, took a stab at the score issue

--- FlappyBird/Form1.cs
+++ FlappyBird/Form1.cs
@@ -12,6 +12,7 @@ namespace FlappyBird
 {
     public partial class Form1 : Form
     {
+        //Global Variables
         bool jumping = false;
         int pipeSpeed = 3;
         int gravity = 3;
@@ -21,10 +22,12 @@ namespace FlappyBird
         public Form1()
         {
             InitializeComponent();
+            //Text displayed at the end of the game. AKA when you die.
             endText1.Text = "Game Over!";
             endText2.Text = "Your final score is: " + Inscore;
             gameDesigner.Text = "Game Designed By Abel Berhane :)";
 
+            //End text is disabled traditionally. 
             endText1.Visible = false;
             endText2.Visible = false;
             gameDesigner.Visible = false;
@@ -32,13 +35,14 @@ namespace FlappyBird
 
         private void gameTimer_Tick(object sender, EventArgs e)
         {
+            //Logic for pipespeed, the player's gravity and the score. They are attached to the timer to make it scroll.
             pipeBottom.Left -= pipeSpeed;
             pipeTop.Left -= pipeSpeed;
             flappyBird.Top += gravity;
             scoreText.Text = " " + Inscore;
 
             
-
+            //Logic for ending the game. If the player intersects with any of the pipes.... end the game.
             if (flappyBird.Bounds.IntersectsWith(ground.Bounds))
             {
                 endGame();
@@ -52,6 +56,7 @@ namespace FlappyBird
                 endGame();
             }
 
+            //Logic for incrementing score. As I pass each pipe, add 1.
             if (pipeBottom.Left< -80)
             {
                 pipeBottom.Left = 1000;
@@ -64,6 +69,7 @@ namespace FlappyBird
             }
         }
 
+        //If the player presses the space bar, allow them to jump and remove 5 pixels of gravity.
         private void inGameKeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Space)
@@ -73,6 +79,7 @@ namespace FlappyBird
             }
         }
 
+        //If the player lets up the space bar, the jump is over and gravity is brought back.
         private void GameKeyUp(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Space)
@@ -82,6 +89,7 @@ namespace FlappyBird
             }
         }
 
+        //When the game ends, set the text to true and show all the labels. 
         private void endGame()
         {
             gameTimer.Stop();
